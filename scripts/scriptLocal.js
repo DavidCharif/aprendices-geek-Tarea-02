8bcafecec9b8282cefff4e01ce1ddb13677ee287
David Charif <davidcharif@hotmail.com>
2022-01-06 15:47:59 -0500

    validadciones

--- scripts/scriptLocal.js
+++ scripts/scriptLocal.js
@@ -7,6 +7,21 @@ let storageSave = () => {
     let key = Date.now()
     key = key.toString()
     key = key.slice(-3);
+    if (name.length < 3 || name.length > 10){
+        return console.log("Nombre demasiado corto/largo")
+    }
+    if (lname.length < 3 || lname.length > 10){
+        return console.log("Apellido demasiado corto/largo")
+    }
+    if (tel.length < 7 || tel.length > 13){
+        return console.log("Telefono entre 7 y 13 caracteres")
+    }
+    if (dir.length < 3 || dir.length > 19){
+        return console.log("Direccion demasiado corto/largo")
+    }
+    if (obser.length < 3){
+        return console.log("Favor escriba mas informacion")
+    }
     const registro = {
         name : name,
         lastName : lname,
